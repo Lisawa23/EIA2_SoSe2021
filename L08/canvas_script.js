3cd2da44a7d74679fb176c91020490d41cac76bf
Lisawa23 <72880380+Lisawa23@users.noreply.github.com>
2021-05-05 17:48:08 +0200

    L08 Updaste

--- L08/canvas_script.js
+++ L08/canvas_script.js
@@ -27,8 +27,10 @@ var L08Canvas;
             let number2 = Math.floor(Math.random() * canvas.width);
             let number3 = Math.floor(Math.random() * canvas.width);
             let number4 = Math.floor(Math.random() * canvas.width);
+            let lineSize = Math.floor(Math.random() * 4);
             crc2.moveTo(number1, number2);
             crc2.lineTo(number3, number4);
+            crc2.lineWidth = lineSize;
         }
         crc2.stroke();
         createCircle();
@@ -55,6 +57,7 @@ var L08Canvas;
         crc2.globalCompositeOperation = "destination-in";
         crc2.arc(posx, posy, radius, posx, 2 * Math.PI);
         crc2.fill();
+        crc2.lineWidth = 1;
         // reset
         crc2.globalCompositeOperation = "source-over";
         createSquare();
@@ -74,8 +77,28 @@ var L08Canvas;
     function createTriangle() {
         positionY += Math.round(Math.random() * (canvas.height / 2));
         positionX += Math.round(Math.random() * (canvas.width / 2));
-        for (let index = 0; index < canvas.height; index++) {
+        for (let index = 0; index < 10; index++) {
+            crc2.beginPath();
+            crc2.moveTo(positionX, positionX);
+            crc2.lineTo(positionX, positionY + 200);
+            crc2.lineTo(positionY + 200, positionY + 200);
+            crc2.closePath();
+            crc2.fillStyle = color2;
+            crc2.fill();
+            crc2.rotate(Math.PI * 90 / 180);
+            crc2.stroke();
         }
+        // createMoreCircles();
     }
+    // function createMoreCircles(): void {
+    //     var xKoord: number = Math.floor(Math.random() * canvas.width);
+    //     var yKoord: number = Math.floor(Math.random() * canvas.width);
+    //     var durchmesser: number = Math.floor(Math.random() * canvas.width / 2);
+    //     console.log(xKoord);
+    //     crc2.arc(xKoord, yKoord, durchmesser, durchmesser, 2 * Math.PI);
+    //     crc2.fillStyle = color;
+    //     crc2.fill();
+    //     crc2.stroke();
+    // }
 })(L08Canvas || (L08Canvas = {}));
 //# sourceMappingURL=canvas_script.js.map
--- L08/canvas_script.ts
+++ L08/canvas_script.ts
@@ -30,8 +30,10 @@ namespace L08Canvas {
         let number2: number = Math.floor(Math.random() * canvas.width);
         let number3: number = Math.floor(Math.random() * canvas.width);
         let number4: number = Math.floor(Math.random() * canvas.width);
+        let lineSize: number = Math.floor(Math.random() * 4);
         crc2.moveTo(number1, number2);
         crc2.lineTo(number3, number4);
+        crc2.lineWidth = lineSize;
     }
     crc2.stroke();
     createCircle();
@@ -55,7 +57,7 @@ namespace L08Canvas {
         gradient.addColorStop(.5, color2);
         gradient.addColorStop(1, color3);
         crc2.fillStyle = gradient;          // set random color
-        crc2.fillRect(x, y, 1, 1);               // draw a pixel
+        crc2.fillRect(x, y, 1, 1);              // draw a pixel
     }
     }
 
@@ -64,9 +66,11 @@ namespace L08Canvas {
     let posy: number = Math.floor(Math.random() * canvas.height);
 
 // removes pixels outside next shape
+    
     crc2.globalCompositeOperation = "destination-in"; 
     crc2.arc(posx, posy, radius, posx, 2 * Math.PI);
     crc2.fill();
+    crc2.lineWidth = 1;
 
 // reset
     crc2.globalCompositeOperation = "source-over"; 
@@ -91,9 +95,31 @@ namespace L08Canvas {
     positionY += Math.round(Math.random() * (canvas.height / 2));
     positionX += Math.round(Math.random() * (canvas.width / 2));  
 
-    for (let index: number = 0; index < canvas.height; index++) {
-        
+    for (let index: number = 0; index < 10; index++) {
+        crc2.beginPath();
+        crc2.moveTo(positionX, positionX);
+        crc2.lineTo(positionX, positionY + 200);
+        crc2.lineTo(positionY + 200, positionY + 200);
+        crc2.closePath();
+      
+        crc2.fillStyle = color2;
+        crc2.fill();
+        crc2.rotate(Math.PI * 90 / 180);
+        crc2.stroke();
     }
+    // createMoreCircles();
     }
+
+    // function createMoreCircles(): void {
+    //     var xKoord: number = Math.floor(Math.random() * canvas.width);
+    //     var yKoord: number = Math.floor(Math.random() * canvas.width);
+    //     var durchmesser: number = Math.floor(Math.random() * canvas.width / 2);
+    //     console.log(xKoord);
+    //     crc2.arc(xKoord, yKoord, durchmesser, durchmesser, 2 * Math.PI);
+    //     crc2.fillStyle = color;
+    //     crc2.fill();
+    //     crc2.stroke();
+    // }
+
 
 }
